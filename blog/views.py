14481515247c11295253be832f91cbad1777f075
Viktor <viktor.raboshchuk@gmail.com>
2021-03-23 13:41:57 +0200

    change function-based views to class-based views

--- blog/views.py
+++ blog/views.py
@@ -54,7 +54,7 @@ class MovieDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        print('check commit')
+
         comments = Comment.objects.filter(post=self.get_object()).order_by('-created_on')
         context['comments'] = comments
         if self.request.user.is_authenticated:
